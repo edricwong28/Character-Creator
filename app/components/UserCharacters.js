3574290a8733302d25656d635d9c00c30ada2411
YETC7 <tyler_726@live.com>
2017-10-01 16:25:44 -0700

    Add user character display component

---
+++ app/components/UserCharacters.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+let testCharacters = [
+	{
+		name:"",
+		age:16,
+		gender:"male"
+	},
+
+	{
+		name:"test",
+		age:16,
+		gender:"male",
+		testProp:""
+	}
+
+];
+
+class UserCharacters extends Component {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+
+export default UserCharacters;
