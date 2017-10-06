d6563b1be4b31db0f5b7c56ccea66246bcade572
Daniel Gallivan <Daniel Gallivan>
2017-10-05 19:47:13 -0700

    first commit of updated file structure

--- app/app.js
+++ app/app.js
@@ -1,49 +1,11 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React { Components } from 'react';
+import ReactDOM { Components } from 'react-dom';
+import Main { Components } from "./components/Main";
 
 ReactDOM.render(
+	<Main />, 
 
-	<div className = 'main-container'>
+document.getElementById("app"))
 
-		<div className="pure-menu pure-menu-horizontal">
-	    	<ul className="pure-menu-list">
-		        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-        		<li class="pure-menu-item"><a href="#" class="pure-menu-link">Log out</a></li>
-        		<li class="pure-menu-item"><a href="#" class="pure-menu-link">Management creator</a></li>
-        	</ul>
-		</div>
 
-		<div className="pure-g">
-			<div className="pure-u-3-5">
-				<div className ="content-wrapper">
-					<div className="content">
-					 <h2 className="content-head is-center">
-					  Welcome back user</h2>
-						<div className="buttons">
-							<a className="button-error pure-button" href="#">Create character</a>
-							<br></br>
-							<br></br>
-							<a className="button-error pure-button" href="#">View other users characters</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="pure-u-1-3">
-				<div className="content-wrapper">
-					<div className="content">
-					 <h2>Creations</h2>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div className="footer">
-			<footer>
-				<p>Management Creator; ©Copyright Daniel G., Edric W., Eric C.</p>
-			</footer>
-		</div>
-
-	</div>, document.getElementById('app'))
-
-module.exports = "app.js"
-// document.write('this works');
+import React, { Components } from 'react'
