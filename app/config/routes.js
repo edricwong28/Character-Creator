cec5cde756642e9f20498fae4095824218b2ab55
Daniel Gallivan <Daniel Gallivan>
2017-10-12 14:27:58 -0700

    corrected issues on firebase and added routes.js

---
+++ app/config/routes.js
@@ -0,0 +1,23 @@
+const React = require('react');
+const router = require('react-router');
+const Route = router.Route;
+const Router = router.Router;
+const hashHistory = router.hashHistory;
+const IndexRoute = route.IndexRoute;
+
+const Welcome = require('../components/Welcome');
+const Main = require('../components/Main');
+const Forum = require('../components/Forum');
+
+module.exports = (
+
+	<Router history={hashHistory}>
+		<Route path="/" component={Main}>
+		
+		<Route path="/Welcome" component={Welcome} />
+
+		<Route path="/Forum" component={Forum} />
+
+		</Route>
+	</Router>
+	);
