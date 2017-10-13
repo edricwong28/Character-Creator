fa004c61c0a8d1b47887032da94d12decdf68634
YETC7 <tyler_726@live.com>
2017-10-13 03:21:14 -0700

    re-add old main

--- app/components/main.js
+++ app/components/main.js
@@ -1,170 +1,53 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import React, { Components } from 'react';
 
-import CharacterForm from "./CharacterForm.js";
-import UserCharacters from "./UserCharacters.js";
-// import ShowCharacter from "./ShowCharacter.js";
+class Main extends React.Component {
+	render() {
+		return(
 
-const firebase = require('firebase');
-const database = require("./firebase.js");
+		<div className = 'main-container'>
 
-// firebase.auth().createUserWithEmailAndPassword("tyler_726@sbcglobal.net", "Testing").then(user => {
-// 	database.ref(`users/${user.uid}`).set({
-// 		key:user.uid,
-// 		email:user.email,
-// 		name:"placehoder",
-// 		createdAt:Date.now()
-// 	});
-// });
+			<div className="pure-menu pure-menu-horizontal">
+		    	<ul className="pure-menu-list">
+			        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+	        		<li class="pure-menu-item"><a href="#" class="pure-menu-link">Log Out</a></li>
+	        		<li class="pure-menu-item" id="title">Management creator</li>
+	        	</ul>
+			</div>
 
-firebase.auth().signInWithEmailAndPassword("tyler_726@sbcglobal.net", "Testing").catch(function(error) {
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	// ...
-}).then(user => {
-	database.ref(`users/${user.uid}`).update({
-		key:user.uid,
-		email:user.email,
-		name:user.email,
-		lastSignedIn:Date.now()
-	});
-});
+			<div className="pure-g">
+				<div className="pure-u-3-5">
+					<div className ="content-wrapper">
+						<div className="content">
+						 <h2 className="content-head is-center">
+						 <b> Welcome back user </b></h2>
+							<div className="buttons">
+								<a className="button-error pure-button" href="createCharacter.js">Create character</a>
+								<br></br>
+								<br></br>
+								<a className="button-error pure-button" href="forum.js">View other users characters</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="pure-u-1-3">
+					<div className="content-wrapper">
+						<div className="content">
+						 <h2>Creations</h2>
+						</div>
+					</div>
+				</div>
+			</div>
 
-const logOutCurrentUser = () => {
+			<div className="footer">
+				<footer>
+					<p>Management Creator; ©Copyright Daniel G., Edric W., Eric C.</p>
+				</footer>
+			</div>
 
-	firebase.auth().signOut().then(function() {
-		  // Sign-out successful.
-		}).catch(function(error) {
-		  // An error happened.
-	});
-}
-
-// firebase.auth().signOut().then(function() {
-// 	  // Sign-out successful.
-// 	}).catch(function(error) {
-// 	  // An error happened.
-// });
-
-// const CurrentUserCharacters = () => {
-// 	if (window.user) {
-// 		return <UserCharacters userKey={window.user}/>
-// 	}
-// 	// return <h1>{window.user}</h1>
-
-// 	return <h1>Loading</h1>
-// }
-
-
-class Main extends Component {
-
-	state = {
-		// userKey:"KDDVKfL7VxbkKOjPNwxFg0pP9cd2",
-		// characterKey:"-KwF2xFzAvVcDBBesku0"
+		</div>
+		);
 	}
-
-	componentWillMount() {
-	    firebase.auth().onAuthStateChanged(function(user) {
-		  	if (user) {
-		    	// User is signed in.
-		    	console.log(user.uid);
-		    	// window.user = user.uid;
-		    	this.setState({userKey:user.uid});
-		  	} 
-
-		 	else {
-		    	// No user is signed in.
-		    	console.log("no user");
-		    	// window.user = null;
-		    	this.setState({userKey:undefined});
-		  	}
-		}.bind(this));
-  	};
-
-  	// render() {
-   //  	return (
-   //  		<div>
-	  //    		<CharacterForm userKey={this.state.userKey}/>
-	  //    		<CharacterForm characterKey={this.state.characterKey} userKey={this.state.userKey}/>
-
-	  //     		<UserCharacters userKey={this.state.userKey}/>
-	  //     		<UserCharacters />
-
-	  //     	</div>
-	  //   );
-  	// }
-
-  	loadUserCharacters() {
-  		let userKey = this.state.userKey;
-  		return <UserCharacters userKey={userKey}/>
-  	};
-
-  	loadCharacterForm() {
-  		let userKey = this.state.userKey;
-  		return <CharacterForm userKey={userKey}/>
-  	}
-
-  	loadPublicCharacters() {
-  		let userKey = this.state.userKey;
-  		return <UserCharacters userKey={userKey} viewing="public"/>
-  	}
-
-  	render() {
-    	return (
-    		<Router>
-	    		<div>
-	    			<Link to="/">Home</Link>
-	    			<button className="btn btn-primary"
-                	onClick={() => logOutCurrentUser()}
-                	> Log-out
-              		</button>
-
-		    		{this.state.userKey === undefined ?(
-		    			<h1>Login Page</h1>
-		    		):(
-		    			
-			    			<div>
-					     		<Switch>
-					     			<Route exact path="/" render={() => (
-					     				<div>
-						     				<Link to="/characters">Your Characters</Link>
-						     				<Link to="/create">New Character</Link>
-						     				<Link to="/public">Public Characters</Link>
-					     				</div>
-					     			)} />
-
-					     			<Route exact path="/characters" render={this.loadUserCharacters.bind(this)} />
-
-					     			<Route exact path="/create" render={this.loadCharacterForm.bind(this)} />
-
-					     			<Route exact path="/public" render={this.loadPublicCharacters.bind(this)} />
-					     		</Switch>
-					      	</div>
-				      	
-		    		)}
-	    		</div>
-    		</Router>
-	    );
-  	}
-
-  	// render() {
-   //  	return (
-   //  		<div>
-   //  			<Router>
-   //  				<Switch>
-   //  					<Route exact path="/" component={UserCharacters} />
-   //  					<Route exact path="/test" component={UserCharacters} />
-   //  					<Route render={() => <h1>Page not found</h1>} />
-   //  				</Switch>
-   //  			</Router>
-	  //     	</div>
-	  //   );
-  	// }
 }
+module.exports = Main;
+	
 
-export default Main;
